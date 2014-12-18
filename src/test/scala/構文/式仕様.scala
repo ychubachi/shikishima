86ad35a437dfdad1d5687d07925cd26a183b5998
Yoshihide Chubachi <yoshi@chubachi.net>
2014-12-18 13:46:24 +0900

    Update

--- src/test/scala/構文/式仕様.scala
+++ src/test/scala/構文/式仕様.scala
@@ -1,7 +1,18 @@
 import org.scalatest.FunSuite
 
+import 構文._
+
 class 式仕様 extends FunSuite {
   test("式の仕様") {
-    assert(true)
+    val x = 真偽値(true)
+    println("abc")
+    assert(x.値 == true)
+
+    val y = 複合式(Array(真偽値(true),真偽値(true)))
+
+    val z = 分岐(
+      真偽値(true),
+      複合式(Array(真偽値(true),真偽値(true))),
+      複合式(Array(真偽値(true),真偽値(true))))
   }
 }
