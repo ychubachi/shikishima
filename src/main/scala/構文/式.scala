deed11fcf28342b71b98af1b1c19767287f13cd2
Yoshihide Chubachi <yoshi@chubachi.net>
2014-12-28 10:46:30 +0900

    Update

--- src/main/scala/構文/式.scala
+++ src/main/scala/構文/式.scala
@@ -2,29 +2,40 @@ package 構文
 
 abstract class 式
 
-case class 文字列(val 値:String) extends 式
-
 case class 複合式(val 値:Array[式]) extends 式 // Compound
+
 case class 分岐(val 条件:式, val 成立:式, val 不成立:式) extends 式
 case class 繰返(val 本体:式) extends 式
 case class 脱出(val 条件:式) extends 式
 
 case class 識別子(val 値:String) extends 式
-case class 代入(val 名前:識別子, val 値:式) extends 式
 
+case class 代入(val 名前:識別子, val 値:式) extends 式
 
 case class 二項演算(val 式1:式, 式2:式, 演算:演算子) extends 式 // Binary
 
+abstract class 定数 extends 式 // Constant
+case class 論理定数(val 値:Boolean) extends 定数 // Boolean_constant
+case class 整数定数(val 値:Int) extends 定数 // Integer_constant
+case class 文字列定数(val 値:String) extends 定数
+
+/*
+ 下記は将来的にはメソッドにする
+ */
 abstract class 演算子 extends 式 // Operator
 abstract class 論理演算子 extends 演算子 // Boolean_op
 abstract class 関係演算子 extends 演算子 // Relational_op
 abstract class 算術演算子 extends 演算子 // Arithmetic_op
 
-/*
-case class 論理積 extends 論理演算 // And
-case class 論理和 extends 論理演算 // Or
- */
+class 論理積 extends 論理演算子 // And
+class 論理和 extends 論理演算子 // Or
 
-abstract class 定数 extends 式 // Constant
-case class 論理定数(val 値:Boolean) extends 定数 // Boolean_constant
-case class 整数定数(val 値:Int) extends 定数 // Integer_constant
+class 超過 extends 関係演算子 // Greater than
+class 同値 extends 関係演算子 // Equal
+class 未満 extends 関係演算子 // Less than
+
+class 加算 extends 算術演算子 // Plus
+class 減算 extends 算術演算子 // Minus
+class 乗算 extends 算術演算子 // Mul
+class 除算 extends 算術演算子 // Div
+class 剰余 extends 算術演算子 // Surplus
--- src/test/scala/構文/式仕様.scala
+++ src/test/scala/構文/式仕様.scala
@@ -3,8 +3,16 @@ import org.scalatest.FunSuite
 import 構文._
 
 class 式仕様 extends FunSuite {
-  test("真偽値") {
-    // new 式
+  test("代入") {
+    val x = 代入(識別子("変数名"), 文字列定数("あいうえお"))
+    assert(x.toString == "代入(識別子(変数名),文字列定数(あいうえお))")
+  }
+
+  test("分岐") {
+    val z = 分岐(
+      論理定数(true), // 条件
+      複合式(Array(論理定数(true),論理定数(true))), // 成立
+      複合式(Array(論理定数(true),論理定数(true)))) // 不成立
   }
 
   test("式の仕様") {
@@ -14,9 +22,5 @@ class 式仕様 extends FunSuite {
 
     val y = 複合式(Array(論理定数(true),論理定数(true)))
 
-    val z = 分岐(
-      論理定数(true),
-      複合式(Array(論理定数(true),論理定数(true))),
-      複合式(Array(論理定数(true),論理定数(true))))
   }
 }
