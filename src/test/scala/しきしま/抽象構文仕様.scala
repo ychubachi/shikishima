51232ab9ad5c3f982dfde4ab8ed8e4255a61db76
Yoshihide Chubachi <yoshi@chubachi.net>
2014-12-29 17:02:29 +0900

    オブジェクト指向型へ

--- src/test/scala/しきしま/抽象構文仕様.scala
+++ src/test/scala/しきしま/抽象構文仕様.scala
@@ -4,29 +4,68 @@ import org.scalatest.FunSuite
 
 import しきしま._
 
-class 式仕様 extends FunSuite {
+class 抽象構文仕様 extends FunSuite {
+  test("定数") {
+    assert(定数(true).値 == true)
+    assert(定数(10).値 == 10)
+    assert(定数("こんにちは").値 == "こんにちは")
+  }
+  
+  test("変数") {
+    assert(識別子("x").名 == "x")
+    assert(識別子("年齢").名 == "年齢")
+  }
+
   test("代入") {
-    val x = 代入(変数("変数名"), 文字列定数("あいうえお"))
-    assert(x.toString == "代入(変数(変数名),文字列定数(あいうえお))")
+    val x = 代入(識別子("自分"), 識別子("名前"), 定数("田中太郎"))
+    assert(x.対象 == 識別子("自分"))
+    assert(x.識別子 == 識別子("名前"))
+    assert(x.式 == 定数("田中太郎"))
+  }
+  
+  test("仮引数") {
+    val x = 仮引数(識別子("縦"), 識別子("横"))
+    assert(x.識別子列.length == 2)
+  }
+  
+  test("関数") {
+    val x = 関数(仮引数(識別子("縦"), 識別子("横")), 定数(0)) // 縦 * 横
+    assert(x.仮引数.識別子列.length == 2)
+    assert(x.本体 == 定数(0))
+    
+    val y = 代入(識別子("四角形"), 識別子("面積"), x) // バインド
+    assert(y.対象 == 識別子("四角形"))
+    assert(y.識別子 == 識別子("面積"))
+    assert(y.式 == x)
+  }
+  
+  test("引数") {
+    val x = 引数()
+    assert(x.式列.length == 0)
+    val y = 引数(定数(true))
+    assert(x.式列.length == 1)
+    val z = 引数(定数(true), 定数(1))
+    assert(z.式列.length == 2)
+  }
+  
+  test("操作") {
+    val x = 操作(定数(1), 識別子("+"), 引数(定数(2))) // 1.+(2)
+    assert(x.対象 == 定数(1))
+    assert(x.識別子 == 識別子("+"))
+    assert(x.引数 == 引数(定数(2)))
+  }
+  
+  test("分岐節") {
+    val x = 分岐節(定数(true), 定数(1))
+    assert(x.条件 == 定数(true))
+    assert(x.式 == 定数(1))
   }
 
   test("分岐") {
-    val z = 分岐(
-      論理定数(true), // 条件
-      複合式(論理定数(true),論理定数(true)), // 成立
-      複合式(論理定数(true),論理定数(true))) // 不成立
-  }
-
-  test("式の仕様") {
-    val x = 論理定数(true)
-    assert(x.値 == true)
-
-    val z = 複合式(論理定数(true),論理定数(true))
+    val z = 分岐(分岐節(定数(true), 定数(1)), 分岐節(定数(false), 定数(0)))
   }
   
-  test("任意定数") {
-    assert(任意定数(true).値 == true)
-    assert(任意定数(10).値 == 10)
-    assert(任意定数("こんにちは").値 == "こんにちは")
+  test("複合式") {
+    val x = 複合式(定数(true),定数(true))
   }
 }
